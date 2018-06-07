e03b82067881608bfe30499d3581383a4a23fd4b
lcr <1558531230@qq.com>
2018-06-07 17:04:09 +0800

    fix bug

--- server/getInfo.js
+++ server/getInfo.js
@@ -10,27 +10,26 @@ let r = (string) => {
 	return string[2] + string[3] + string[0] + string[1];
 };
 
-let data = [];
-let info = fs.readFileSync(config.infoFilePath, 'utf-8');
-info = info.split('\n');
-for (let i of info) {
-	if (i.length) {
-		let divideI = i.split(',');
-		let temp = {};
-		for (let j of divideI) {
-			let divideJ = j.split(':');
-			if (divideJ[0].length === 8) {
-				temp[r(divideJ[0])] = divideJ[1];
-			}
-			else {
-				temp[divideJ[0]] = divideJ[1];
-			}
-		}
-		data.push(temp);
-	}
-}
-
 module.exports = (id) => {
+	let data = [];
+	let info = fs.readFileSync(config.infoFilePath, 'utf-8');
+	info = info.split('\n');
+	for (let i of info) {
+		if (i.length) {
+			let divideI = i.split(',');
+			let temp = {};
+			for (let j of divideI) {
+				let divideJ = j.split(':');
+				if (divideJ[0].length === 8) {
+					temp[r(divideJ[0])] = divideJ[1];
+				}
+				else {
+					temp[divideJ[0]] = divideJ[1];
+				}
+			}
+			data.push(temp);
+		}
+	}
 	for (let i of data) {
 		let keys = Object.keys(i);
 		if (keys[0] === id) {
--- simulator/index.js
+++ simulator/index.js
@@ -7,75 +7,78 @@ const fs = require('fs');
 const config = require('../config/config');
 let nodes = [];
 let nodesType = [];
-let typeNumber = [13,9,7,7,18,10,5,5,21,6,10,10];
-for (let i = 0;i<8;++i){
+let typeNumber = [13, 9, 7, 7, 18, 10, 5, 5, 21, 6, 10, 10];
+for (let i = 0; i < 8; ++i) {
 	let temp = [];
-	for (let j = 0;j<6;++j){
+	for (let j = 0; j < 6; ++j) {
 		temp.push(`8666683506aacd900bbd5a74ac4edf${i}${j}`);
 	}
 	nodes.push(temp);
 }
 let nodesString = '';
-for (let i = 0;i<8;++i){
-	for (let j = 0;j<6;++j){
+for (let i = 0; i < 8; ++i) {
+	for (let j = 0; j < 6; ++j) {
 		nodesString = nodesString + nodes[i][j] + '\n';
 	}
 }
-fs.writeFileSync('../test/nodes.txt',nodesString);
+fs.writeFileSync('../test/nodes.txt', nodesString);
 let nodesTypeString = '';
-for (let i = 0;i<8;++i){
+for (let i = 0; i < 8; ++i) {
 	let temp = [];
-	for (let j = 0;j<6;++j){
-		temp.push(parseInt((Math.random()*12).toString())+1);
+	for (let j = 0; j < 6; ++j) {
+		temp.push(parseInt((Math.random() * 12).toString()) + 1);
 		nodesTypeString = nodesTypeString + nodes[i][j] + `,${temp[j]}\n`;
 	}
 	nodesType.push(temp);
 }
-fs.writeFileSync('../test/type.txt',nodesTypeString);
+fs.writeFileSync('../test/type.txt', nodesTypeString);
 let nodesTypeAtrribution = '';
-for (let i = 0;i<8;++i){
-	for (let j = 0;j<6;++j){
+for (let i = 0; i < 8; ++i) {
+	for (let j = 0; j < 6; ++j) {
 		let temp = `${nodes[i][j]}:${nodesType[i][j].toString(16)}000000,`;
-		for (let z = 0;z < typeNumber[nodesType[i][j] - 1];++z){
-			let n = (145+z).toString(16);
+		for (let z = 0; z < typeNumber[nodesType[i][j] - 1]; ++z) {
+			let n = (145 + z).toString(16);
 			temp += `${n}020000:FFFFFFFF,`;
 		}
 		nodesTypeAtrribution += temp;
 		nodesTypeAtrribution += '\n';
 	}
 }
-fs.writeFileSync('../test/jinlei.txt',nodesTypeAtrribution);
+fs.writeFileSync('../test/jinlei.txt', nodesTypeAtrribution);
+
 class edge {
-	constructor(s,e,i){
+	constructor(s, e, i) {
 		this.start = s;
 		this.end = e;
 		this.is = i;
 	}
 }
 
-class graph{
-	constructor(){
+class graph {
+	constructor() {
 		this.edges = [];
-		for (let i = 0;i<48;++i){
-			if((i+1)%6 !== 0){
-				if(parseInt(i/6)%2 === 0)this.edges.push(new edge(i,i+1,true));
+		for (let i = 0; i < 48; ++i) {
+			if ((i + 1) % 6 !== 0) {
+				if (parseInt(i / 6) % 2 === 0) this.edges.push(new edge(i, i + 1, true));
 			}
-			if(i+6<48){
-				this.edges.push(new edge(i,i+6,true));
+			if (i + 6 < 48) {
+				this.edges.push(new edge(i, i + 6, true));
 			}
 		}
 	}
-	randomGenerate(){
-		for (let i = 0;i < this.edges.length;++i){
+
+	randomGenerate() {
+		for (let i = 0; i < this.edges.length; ++i) {
 			this.edges[i].is = !!Math.round(Math.random());
 			//this.edges[i].is = true;
 		}
 	}
-	getLinkNode(nodePoint){
+
+	getLinkNode(nodePoint) {
 		let n = [];
-		for (let i of this.edges){
-			if(i.start === nodePoint && i.is) n.push(i.end);
-			if(i.end === nodePoint && i.is) n.push(i.start);
+		for (let i of this.edges) {
+			if (i.start === nodePoint && i.is) n.push(i.end);
+			if (i.end === nodePoint && i.is) n.push(i.start);
 		}
 		return n;
 	}
@@ -86,21 +89,21 @@ let g = new graph();
 function generateTopo() {
 	let nodesTopoString = '';
 	g.randomGenerate();
-	for (let i = 0;i<8;++i){
-		for (let j = 0;j<6;++j){
-			let linkNode = g.getLinkNode(i*6+j);
+	for (let i = 0; i < 8; ++i) {
+		for (let j = 0; j < 6; ++j) {
+			let linkNode = g.getLinkNode(i * 6 + j);
 			nodesTopoString = nodesTopoString + nodes[i][j] + ',';
-			for (let i of linkNode){
-				nodesTopoString = nodesTopoString + nodes[parseInt(i/6)][i%6] + ',';
+			for (let i of linkNode) {
+				nodesTopoString = nodesTopoString + nodes[parseInt(i / 6)][i % 6] + ',';
 			}
-			for (let i = 0;i < 6 - linkNode.length;++i){
+			for (let i = 0; i < 6 - linkNode.length; ++i) {
 				nodesTopoString = nodesTopoString + config.leftFlag + ',';
 			}
 			nodesTopoString += '\n';
 		}
 	}
-	fs.writeFileSync('../test/topo.txt',nodesTopoString);
-	console.log((new Date()).toLocaleTimeString()+'topo.txt生成成功！');
+	fs.writeFileSync('../test/topo.txt', nodesTopoString);
+	console.log((new Date()).toLocaleTimeString() + 'topo.txt生成成功！');
 }
 
-setInterval(generateTopo,2000);
+setInterval(generateTopo, 2000);
